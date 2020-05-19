bc4e759539eb137564914ef82785fba761104bf2
AMHOL <andyholland1991@aol.com>
2020-05-19 16:26:27 +0800

    Implement PR::Pin.subscriptions.find

--- lib/pr/pin/repositories/subscriptions.rb
+++ lib/pr/pin/repositories/subscriptions.rb
@@ -3,6 +3,12 @@ module PR
     module Repositories
       class Subscriptions < ROM::Repository[:subscriptions]
         include Concerns::Common
+
+        def find(token)
+          API::Result.wrap(root) do
+            root.append_path(token).one
+          end
+        end
       end
     end
   end
---
+++ spec/shared/context/responses/common/find/not_found.rb
@@ -0,0 +1,9 @@
+RSpec.shared_context 'responses.find.not_found' do
+  let(:error_response) do
+    {
+      error: 'not_found',
+      error_description: 'The requested resource could not be found.'
+    }
+  end
+  let(:error_response_json) { JSON.dump(error_response) }
+end
