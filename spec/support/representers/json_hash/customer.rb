a9b1db0f6b9ebb6e7304c10760bc305c759d70d7
AMHOL <andyholland1991@aol.com>
2020-10-16 16:06:07 +0800

    Add optional attributes to customers relation

--- spec/support/representers/json_hash/customer.rb
+++ spec/support/representers/json_hash/customer.rb
@@ -5,6 +5,11 @@ module Representers
         {
           token: customer.token,
           email: customer.email,
+          first_name: customer.first_name,
+          last_name: customer.last_name,
+          phone_number: customer.phone_number,
+          company: customer.company,
+          notes: customer.notes,
           created_at: customer.created_at,
           card: customer.card
         }
